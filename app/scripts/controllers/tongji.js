45cb729c1bfab1da70b29128aff66afed10581cc
songtao <songtao.zhang@cloudisan.com>
2014-08-18 15:31:50 +0800

    shijuan and kaoshi tongji paging done

--- app/scripts/controllers/tongji.js
+++ app/scripts/controllers/tongji.js
@@ -25,7 +25,13 @@ define(['jquery', 'underscore', 'angular', 'config'], function ($, _, angular, c
           queryShiJuan = baseTjAPIUrl + 'query_shijuan?token=' + token + '&caozuoyuan=' + caozuoyuan
             + '&jigouid=' + jigouid + '&lingyuid=' + lingyuid, //查询试卷数据
           queryKaoShengBase = baseTjAPIUrl + 'query_kaosheng?token=' + token, //查询考生数据
-          queryTiMuBase = baseTjAPIUrl + 'query_timu?token=' + token; //查询题目数据
+          queryTiMuBase = baseTjAPIUrl + 'query_timu?token=' + token, //查询题目数据
+          dataNumOfPerPage = 10, //每页显示多少条数据
+          paginationLength = 11, //分页部分，页码的长度，目前设定为11
+          pagesArr = [], //定义考试页码数组
+          tjNeedData, //存放查询出来的统计数数据
+          lastPage; //符合条件的考试一共有多少页
+
 
         /**
          * 信息提示函数
@@ -48,9 +54,17 @@ define(['jquery', 'underscore', 'angular', 'config'], function ($, _, angular, c
          * 显示考试统计列表
          */
         $scope.showKaoShiTjList = function(){
+          pagesArr = [];
+          tjNeedData = '';
           $http.get(queryKaoShi).success(function(data){
             if(data.length){
-              $scope.tjKaoShiData = data;
+              tjNeedData = data;
+              lastPage = Math.ceil(data.length/dataNumOfPerPage); //得到所有考试的页码
+              $scope.lastPageNum = lastPage;
+              for(var i = 1; i <= lastPage; i++){
+                pagesArr.push(i);
+              }
+              $scope.tjPaging();
             }
             else{
               alertInfFun('err', data.error);
@@ -64,9 +78,17 @@ define(['jquery', 'underscore', 'angular', 'config'], function ($, _, angular, c
          * 显示试卷统计列表
          */
         $scope.showShiJuanTjList = function(){
+          pagesArr = [];
+          tjNeedData = '';
           $http.get(queryShiJuan).success(function(data){
             if(data.length){
-              $scope.tjShiJuanData = data;
+              tjNeedData = data;
+              lastPage = Math.ceil(data.length/dataNumOfPerPage); //得到所有考试的页码
+              $scope.lastPageNum = lastPage;
+              for(var i = 1; i <= lastPage; i++){
+                pagesArr.push(i);
+              }
+              $scope.tjPaging();
             }
             else{
               alertInfFun('err', data.error);
@@ -191,7 +213,31 @@ define(['jquery', 'underscore', 'angular', 'config'], function ($, _, angular, c
               $scope.showShiJuanTjList(); //试卷详情如果是由试卷统计
             }
           }
-        }
+        };
+
+        /**
+         * 考试的分页数据//
+         */
+        $scope.tjPaging = function(pg){
+          //得到分页数组的代码
+          var currentPage = $scope.currentPage = pg ? pg : 1;
+          if(lastPage <= paginationLength){
+            $scope.tjPages = pagesArr;
+          }
+          if(lastPage > paginationLength){
+            if(currentPage > 0 && currentPage <= 6 ){
+              $scope.tjPages = pagesArr.slice(0, paginationLength);
+            }
+            else if(currentPage > lastPage - 5 && currentPage <= lastPage){
+              $scope.tjPages = pagesArr.slice(lastPage - paginationLength);
+            }
+            else{
+              $scope.tjPages = pagesArr.slice(currentPage - 5, currentPage + 5);
+            }
+          }
+          //查询数据的代码
+          $scope.tjData = tjNeedData.slice((currentPage-1)*10, currentPage*10);
+        };
 
     }]);
 });
